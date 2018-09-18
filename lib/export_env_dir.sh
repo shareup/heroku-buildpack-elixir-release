2e9b6d3928a34e1d4c3e33a51549a6a6a1f6d842
Nathan Herald <me@nathanherald.com>
2018-09-18 13:18:51 -0700

    Simply env stuff more

--- lib/export_env_dir.sh
+++ lib/export_env_dir.sh
@@ -6,10 +6,8 @@ export_env_dir() {
   ls $env_dir
 
   if [ -d "$env_dir" ]; then
-    local whitelist_regex="$2"
-    local blacklist_regex="${3:-^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH)$}"
     for e in $(ls $env_dir); do
-      echo "$e" | grep -E "$whitelist_regex" | grep -qvE "$blacklist_regex" && export $e=$(cat $env_dir/$e)
+      echo "$e" | grep -qvE '^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH)$' && export $e=$(cat $env_dir/$e)
     done
   fi
 }
