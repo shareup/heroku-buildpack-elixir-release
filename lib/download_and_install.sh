6f1c67ecebd6ddd014b47e57ad0fa6e43b0f84d9
Nathan Herald <me@nathanherald.com>
2018-09-24 17:58:41 -0700

    Use the new download urls for erlang and elixir
    And have better default versions.

--- lib/download_and_install.sh
+++ lib/download_and_install.sh
@@ -5,35 +5,24 @@ download_and_install_erlang() {
   local install_dir="$2"
   local cache_dir="$3"
 
+  local region="${S3_REGION:-us-west-1}"
+  local bucket="${S3_BUCKET:-heroku-buildpack-elixir-release-default}"
+
   mkdir -p "$install_dir"
   mkdir -p "$cache_dir"
 
-  local filename="otp_src_$otp_version.tar.gz"
-  local url="http://erlang.org/download/$filename"
+  local filename="OTP-$otp_version.tar.gz"
+  local url="https://s3-$region.amazonaws.com/$bucket/$filename"
   local tarpath="$cache_dir/$filename"
-  local build_dir="$cache_dir/build"
-  export ERL_TOP="$build_dir"
-
-  mkdir -p "$build_dir"
 
   if [ ! -f "$tarpath" ]; then
     rm -rf "$cache_dir/*"
     rm -rf "$install_dir/*"
 
     echo "Downloading OTP $otp_version"
-    curl -# "$url" -o "$tarpath" || (echo "Unable to download erlang" && exit 1)
+    curl -# -f "$url" -o "$tarpath" || (echo "Unable to download erlang" && exit 1)
 
-    tar xzf "$tarpath" -C "$build_dir" --strip-components=1
-
-    echo "Compiling erlang, this could take a few minutes"
-
-    cd $build_dir
-    ./configure
-    make
-    make RELEASE_ROOT="$install_dir" release
-    cd -
-
-    $install_dir/Install -minimal "$install_dir"
+    tar xf "$tarpath" -C "$install_dir" --strip-components=1
 
     chmod +x "$install_dir/bin/*"
   else
@@ -49,28 +38,24 @@ download_and_install_elixir() {
   local install_dir="$3"
   local cache_dir="$4"
 
+  local region="${S3_REGION:-us-west-1}"
+  local bucket="${S3_BUCKET:-heroku-buildpack-elixir-release-default}"
+
   mkdir -p "$install_dir"
   mkdir -p "$cache_dir"
 
-  local filename="$elixir_version-otp-$otp_version.zip"
-  local url="https://repo.hex.pm/builds/elixir/v$filename"
+  local filename="elixir-$elixir_version-OTP-$otp_version.tar.gz"
+  local url="https://s3-$region.amazonaws.com/$bucket/$filename"
   local tarpath="$cache_dir/$filename"
-  local build_dir="$cache_dir/build/"
 
   if [ ! -f "$tarpath" ]; then
     rm -rf "$cache_dir/*"
     rm -rf "$install_dir/*"
 
     echo "Downloading elixir $elixir_version"
-    curl -# "$url" -o "$tarpath"
+    curl -# -f "$url" -o "$tarpath"
 
-    if [ "$?" -ne "0" ]; then
-      echo "Unable to download an elixir for OTP $otp_version, falling back to generic elixir version"
-      local fallback_url="https://repo.hex.pm/builds/elixir/v$elixir_version.zip"
-      curl -# "$fallback_url" -o "$tarpath" || (echo "Unable to download elixir" && exit 1)
-    fi
-
-    unzip -q "$tarpath" -d "$install_dir/"
+    tar xf "$tarpath" -d "$install_dir/"
 
     chmod +x "$install_dir/bin/*"
   else
@@ -90,15 +75,15 @@ download_and_install_node() {
   mkdir -p "$cache_dir"
 
   local os="$(uname | tr A-Z a-z)"
-  local filename="node-v$version-$os-x86.tar.gz"
-  local url="http://s3pository.heroku.com/node/v$version/$filename"
+  local filename="node-v$version-linux-x64.tar.xz"
+  local url="https://nodejs.org/dist/v$version/$filename"
   local tarpath="$cache_dir/$filename"
 
   if [ ! -f "$tarpath" ]; then
     echo "Downloading node"
-    curl -# "$url" -o "$tarpath" || (echo "Unable to download node" && exit 1)
+    curl -# -f "$url" -o "$tarpath" || (echo "Unable to download node" && exit 1)
 
-    tar xzf "$tarpath" -C "$install_dir" --strip-components=1
+    tar xf "$tarpath" -C "$install_dir" --strip-components=1
 
     chmod +x "$install_dir/bin/*"
   else
