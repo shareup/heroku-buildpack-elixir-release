7af510b8fa66bf817029dbf77b9bbc544307a2c0
Nathan Herald <me@nathanherald.com>
2018-10-08 16:09:45 -0700

    We don't need a PS1

--- lib/export_env_dir.sh
+++ lib/export_env_dir.sh
@@ -6,7 +6,7 @@ export_env_dir() {
   if [ -d "$env_dir" ]; then
     for e in $(ls $env_dir); do
       echo "$e"
-      echo "$e" | grep -qv '^PATH$\|^GIT_DIR$\|^CPATH$\|^CPPATH$\|^LD_PRELOAD$\|^LIBRARY_PATH$' && export $e=$(cat $env_dir/$e)
+      echo "$e" | grep -qv '^PS1$\|^PATH$\|^GIT_DIR$\|^CPATH$\|^CPPATH$\|^LD_PRELOAD$\|^LIBRARY_PATH$' && export $e=$(cat $env_dir/$e)
     done
   fi
 }
