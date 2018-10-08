05b83ccba4982f735bf592a84b0bd102614ace6c
Nathan Herald <me@nathanherald.com>
2018-10-08 16:12:36 -0700

    Don't export env var names

--- lib/export_env_dir.sh
+++ lib/export_env_dir.sh
@@ -5,7 +5,6 @@ export_env_dir() {
 
   if [ -d "$env_dir" ]; then
     for e in $(ls $env_dir); do
-      echo "$e"
       echo "$e" | grep -qv '^PS1$\|^PATH$\|^GIT_DIR$\|^CPATH$\|^CPPATH$\|^LD_PRELOAD$\|^LIBRARY_PATH$' && export $e=$(cat $env_dir/$e)
     done
   fi
