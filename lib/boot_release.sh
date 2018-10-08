fec7195253219d76201bcce35b919842c06f7b1c
Nathan Herald <me@nathanherald.com>
2018-10-08 15:34:47 -0700

    Only need the app name

--- lib/boot_release.sh
+++ lib/boot_release.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 MIX_APP_NAME=$(cat /app/MIX_APP_NAME)
-MIX_APP_VERSION=$(cat /app/MIX_APP_VERSION)
 
-/app/_build/$MIX_ENV/rel/$MIX_APP_NAME/releases/$MIX_APP_VERSION/$MIX_APP_NAME.sh foreground
+/app/_build/$MIX_ENV/rel/$MIX_APP_NAME/bin/$MIX_APP_NAME foreground
