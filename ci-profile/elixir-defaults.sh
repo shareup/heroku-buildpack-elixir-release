05f40fff8b3f3950c2c0b9cb8a4acce3069d0e09
Nathan Herald <me@nathanherald.com>
2018-10-08 17:21:26 -0700

    Make sure the path is setup for elixir

--- ci-profile/elixir-defaults.sh
+++ ci-profile/elixir-defaults.sh
@@ -2,8 +2,10 @@
 
 export PATH="$HOME/.heroku/elixir/bin:$HOME/.heroku/erlang/bin:$HOME/.heroku/node/bin:$HOME/bin:$PATH"
 export NODE_HOME="$HOME/.heroku/node"
-export MIX_HOME="/app/.mix"
-export HEX_HOME="/app/.hex"
-export LANG="${LANG:-en_US.utf8}"
-export LC_CTYPE="${LC_TYPE:-en_US.utf8}"
-export MIX_ENV=${MIX_ENV:-test}
+export MIX_HOME="$HOME/.mix"
+export HEX_HOME="$HOME/.hex"
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US:en
+export LC_ALL=en_US.UTF-8
+export LC_CTYPE=en_US.utf8
+export MIX_ENV=test
